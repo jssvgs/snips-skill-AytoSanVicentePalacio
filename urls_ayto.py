95290bd2e5f6f6f3c9be55956594c99ab008f28e
jssvgs <jssvgs@gmail.com>
2019-07-16 10:46:47 +0200

    primer intento

---
+++ urls_ayto.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+nombre_item = list()
+url_item = list()
+
+nombre_item.append("Telefono")
+url_item.append("https://sanvicentedelpalacio.ayuntamientosdevalladolid.es/contacto")
